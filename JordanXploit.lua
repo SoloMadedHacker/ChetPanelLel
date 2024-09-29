ba7d4195a57e8fdb3ed0d17ad4c9e0a7cfa873ad
SoloMadedHacker <jqjeiw2@gmail.com>
2024-09-29 11:03:24 +0800

    JordanXploit.lua
    This a cheat panel roblox

---
+++ JordanXploit.lua
@@ -0,0 +1,5 @@
+-- You really thought you could steal my script? 
+-- Blud trying to say nah
+-- Well shut up 
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SoloMadedHacker/HexPanel/main/finish.lua"))()
